46485119deb5ebbebc8bb8486845ed080bab8a90
MattyxD22 <63449613+MattyxD22@users.noreply.github.com>
2024-05-31 00:50:01 +0200

    test workflow 5

--- server.js
+++ server.js
@@ -1,49 +1,43 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv-flow").config();
 const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_URI);
+
 const cookieParser = require("cookie-parser");
-const express = require("express");
-const cors = require("cors");
+
 const swaggerUI = require("swagger-ui-express");
 const yamlJS = require("yamljs");
 
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cookieParser());
 
-
+// Use built-in middleware for parsing requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(cors());
 
-
-mongoose.connect(process.env.DB_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+const PORT = process.env.PORT || 3000;
 
 // Define routes
 const carsRoute = require("./routes/carRoute");
 const usersRoute = require("./routes/userRoute");
+
 app.use("/api/cars", carsRoute);
 app.use("/api/users", usersRoute);
-
 
 const swaggerDefinition = yamlJS.load("./swagger.yaml");
 app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDefinition));
 
 
+
+app.listen(PORT, async () => {
+  console.log("Server Listening on PORT:", PORT);
+});
 app.get("/api/health-check", (req, res) => {
   res.status(200).send({ message: "Health check was successful!" });
 });
 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server listening on PORT:", PORT);
-});
-
-module.exports = app;
+module.exports = app
