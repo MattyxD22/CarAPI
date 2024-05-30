6619b81531bc051cb8569ffe3bb9071ebcbab77e
MattyxD22 <63449613+MattyxD22@users.noreply.github.com>
2024-05-31 00:47:09 +0200

    test workflow 3

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv-flow").config();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
@@ -10,15 +9,14 @@ const yamlJS = require("yamljs");
 const app = express();
 app.use(cookieParser());
 
-// Use built-in middleware for parsing requests
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// CORS configuration
-app.use(cors());
-// Add more specific CORS configuration if needed
 
-// MongoDB connection
+app.use(cors());
+
+
 mongoose.connect(process.env.DB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
@@ -33,16 +31,16 @@ const usersRoute = require("./routes/userRoute");
 app.use("/api/cars", carsRoute);
 app.use("/api/users", usersRoute);
 
-// Swagger documentation setup
+
 const swaggerDefinition = yamlJS.load("./swagger.yaml");
 app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDefinition));
 
-// Health check endpoint
+
 app.get("/api/health-check", (req, res) => {
   res.status(200).send({ message: "Health check was successful!" });
 });
 
-// Start the server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server listening on PORT:", PORT);
